7bf163531529187a90b76ed7ccb025366b34fccd
liz <lizfernanda.rq@gmail.com>
2021-11-28 17:19:57 -0400

    catalogo mejorado

--- resources/views/carrito/estado.blade.php
+++ resources/views/carrito/estado.blade.php
@@ -2,174 +2,77 @@
 
 @section('content')
 
-
-<!-- <div class="breadcrumb_section bg_gray page-title-mini">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-md-6">
-                <div class="page-title">
-                    <h1>AgroShop</h1>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <ol class="breadcrumb justify-content-md-end" itemscope="" itemtype="http://schema.org/BreadcrumbList">
-                    <li class="breadcrumb-item" itemprop="itemListElement" itemscope=""
-                        itemtype="http://schema.org/ListItem">
-                        <meta itemprop="position" content="1">
-                        <a href="https://shopwise.botble.com" itemprop="item" title="Home">
-                            Home
-                            <meta itemprop="name" content="Home">
-                        </a>
-                    </li>
-                    <li class="breadcrumb-item active">AgroShop</li>
-                </ol>
-            </div>
-        </div>
-    </div>
-</div> -->
-
-<div class="section">
-    <div class="container">
-        <!--       <form class="form--shopping-cart" method="post" action="javascript:void(0);"> -->
-        <input type="hidden" name="_token" value="J6koTs6uk1YiqqprBhcilmFsjelUTZClSF1NieIZ">
-        <div class="row">
-            <div class="col-12">
-                <div class="table-responsive shop_cart_table">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th class="product-thumbnail">Imagen</th>
-                                <th class="product-name">Producto</th>
-                                <th class="product-price">Precio</th>
-                                <th class="product-quantity">Cantidad</th>
-                                <th class="product-subtotal">Total</th>
-                                <th class="product-remove">Eliminar</th>
+<div class="container mt-5 ctl-bg dtl">
+      <div class="row">
+        <aside class="col-lg-9">
+            <div class="card">
+                <div class="table-responsive">
+                    <table class="table table-borderless table-shopping-cart">
+                        <thead class="text-muted">
+                            <tr class="small text-uppercase">
+                                <th scope="col">Product</th>
+                                <th scope="col" width="120">Quantity</th>
+                                <th scope="col" width="120">Price</th>
+                                <th scope="col" class="text-right d-none d-md-block" width="200"></th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($items as $item)
+                               @foreach ($items as $item)
                             <tr>
-                                <td class="product-thumbnail">
-                                    <a href="#">
-                                        {{--<img src="{{ asset($_ENV['IMAGEN_PROD_AGRO'].$item->attributes->image ) }}">--}}
-                                        <img src="{{ asset($_ENV['IMAGEN_PROD_AGRO'].$item->attributes->image ) }}">
-                                    </a>
+                                <td>
+                                    <figure class="itemside align-items-center">
+                                        <div class="aside"><img src="{{ asset($_ENV['IMAGEN_PROD_AGRO'].$item->attributes->image ) }}" class="img-sm"></div>
+                                        <figcaption class="info"> <a href="#" class="title text-dark" data-abc="true">{{$item->name}}</a>
+            
+                                        </figcaption>
+                                    </figure>
                                 </td>
-                                <td class="product-name" data-title="Product">
-                                    <a href="#" title="Headphone Ultra Bass"> {{$item->name}}</a>
+                                <td> <select class="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                    </select> </td>
+                                <td>
+                                    <div class="price-wrap"> <var class="price">Bs.- {{$item->price}}</var><!--  <small class="text-muted"> $9.20 each </small> --> </div>
                                 </td>
-                                <td class="product-price" data-title="Price">
-                                    <div class="product__price ">
-                                        <span> {{$item->price}} </span>
-                                    </div>
-                                    <input type="hidden" name="items[e2aab0e2b7e45be057e0c70f299bace9][rowId]"
-                                        value="e2aab0e2b7e45be057e0c70f299bace9">
-                                </td>
-
-                                <td class="product-quantity" data-title="Quantity">
-                                    <div class="quantity">
-
-                                        <form action="{{ route('carrito.updateItem') }}" method="POST"
-                                            enctype="multipart/form-data">
-                                            @csrf
-                                            <input type="hidden" value="{{ $item->id }}" name="id">
-                                            <input type="hidden" value="{{ $item->name }}" name="nombre">
-                                            <input type="hidden" value="{{ $item->price }}" name="precio">
-                                            <!--    <input type="hidden" value="{{ $item->image }}" name="imagen"> -->
-                                            <input type="hidden" value="-1" name="quantity">
-                                            <button value="-" class="minus"> - </button>
-                                        </form>
-                                        <input type="text" value=" {{$item->quantity}}" title="Qty" class="qty" size="4"
-                                            name="items[e2aab0e2b7e45be057e0c70f299bace9][values][qty]">
-
-                                        <form action="{{ route('carrito.updateItem') }}" method="POST"
-                                            enctype="multipart/form-data">
-                                            @csrf
-                                            <input type="hidden" value="{{ $item->id }}" name="id">
-                                            <input type="hidden" value="{{ $item->name }}" name="nombre">
-                                            <input type="hidden" value="{{ $item->price }}" name="precio">
-                                            <!--    <input type="hidden" value="{{ $item->image }}" name="imagen"> -->
-                                            <input type="hidden" value="1" name="quantity">
-                                            <button value="+" class="plus"> + </button>
-                                        </form>
-
-                                    </div>
-                                </td>
-                                <td class="product-subtotal" data-title="Total"> {{$item->getPriceSum()}} </td>
-                                <td class="product-remove" data-title="Remove">
-
-                                    <form action="{{ route('carrito.eliminar') }}" method="POST"
-                                        enctype="multipart/form-data">
-                                        @csrf
-                                        <input type="hidden" value="{{ $item->id }}" name="id">
-                                        <button class="remove-cart-button"><i class="ti-close"></i>
-                                        </button>
-                                    </form>
-
-                                </td>
+                                <td class="text-right d-none d-md-block"> <a data-original-title="Save to Wishlist" title="" href="" class="btn btn-light" data-toggle="tooltip" data-abc="true"> <i class="fa fa-heart"></i></a> <a href="" class="btn btn-light" data-abc="true"> Remove</a> </td>
                             </tr>
-                            @endforeach
+                                    @endforeach
                         </tbody>
-                        <tfoot>
-                            <tr>
-                                <!--  <td colspan="6" class="px-0">
-                                        <div class="row no-gutters align-items-center">
-                                            <div class="col-lg-4 col-md-6 mb-3 mb-md-0">
-                                                <div class="coupon field_form input-group form-coupon-wrapper">
-                                                    <input type="text" name="coupon_code" value=""
-                                                        class="form-control form-control-sm coupon-code"
-                                                        placeholder="Enter Coupon Code...">
-                                                    <div class="input-group-append">
-                                                        <button class="btn btn-fill-out btn-sm btn-apply-coupon-code"
-                                                            type="button"
-                                                            data-url="https://shopwise.botble.com/coupon/apply">Apply
-                                                            Coupon</button>
-                                                    </div>
-                                                </div>
-                                                <div class="coupon-error-msg text-left">
-                                                    <small><span class="text-danger"></span></small>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td> -->
-                            </tr>
-                        </tfoot>
                     </table>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <div class="medium_divider"></div>
-                <div class="divider center_icon"><i class="ti-shopping-cart-full"></i></div>
-                <div class="medium_divider"></div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="border p-3 p-md-4">
-                    <div class="heading_s1 mb-3">
-                        <h6>Cart Totals</h6>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <tbody>
-
-
-                                <tr>
-                                    <td class="cart_total_label">Total (no incluye envios)</td>
-                                    <td class="cart_total_amount"><strong>{{\Cart::getTotal()}}</strong></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <button type="submit" class="btn btn-fill-out" name="checkout">Proceder compra</button>
+        </aside>
+        <aside class="col-lg-3">
+            <!-- <div class="card mb-3">
+                <div class="card-body">
+                    <form>
+                        <div class="form-group"> <label>Have coupon?</label>
+                            <div class="input-group"> <input type="text" class="form-control coupon" name="" placeholder="Coupon code"> <span class="input-group-append"> <button class="btn btn-primary btn-apply coupon">Apply</button> </span> </div>
+                        </div>
+                    </form>
+                </div>
+            </div> -->
+            <div class="card">
+                <div class="card-body">
+                    <dl class="dlist-align">
+                        <dt>Total price:</dt>
+                        <dd class="text-right ml-3">$69.97</dd>
+                    </dl>
+                    <dl class="dlist-align">
+                        <dt>Discount:</dt>
+                        <dd class="text-right text-danger ml-3">- $10.00</dd>
+                    </dl>
+                    <dl class="dlist-align">
+                        <dt>Total:</dt>
+                        <dd class="text-right text-dark b ml-3"><strong>$59.97</strong></dd>
+                    </dl>
+                    <hr> <a href="#" class="btn btn-out btn-primary btn-square btn-main" data-abc="true"> Make Purchase </a> <a href="#" class="btn btn-out btn-success btn-square btn-main mt-2" data-abc="true">Continue Shopping</a>
                 </div>
             </div>
-        </div>
-        <!--       </form> -->
-
+        </aside>
     </div>
 </div>
-
+</div>
 @endsection
