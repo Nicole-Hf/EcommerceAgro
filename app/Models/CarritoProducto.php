f3af2fa6b0aee3ae5f5b401fb5de59ab514c7a61
liz <lizfernanda.rq@gmail.com>
2021-12-18 18:23:41 -0400

    carrito en bd arreglado conflicto

--- app/Models/CarritoProducto.php
+++ app/Models/CarritoProducto.php
@@ -31,31 +31,4 @@ class CarritoProducto extends Model
     {
         return $this->where(['carrito_id' => $carrito])->get();
     }
-    public static function carritoCliente()
-    {
-        if (auth()->check()) {
-            $idUser = auth()->user()->id;
-            $cliente = \App\Models\Cliente::where('user_id', $idUser)->first();
-            $carrito = \App\Models\Carrito::where('cliente_id', $cliente->id)->first();
-
-            $resultado = CarritoProducto::join("productos", "productos.id", "=", "carritos_productos.producto_id")
-                ->select("carritos_productos.*", "productos.nombre", "productos.imagen", "productos.precio")
-                ->where("carritos_productos.carrito_id", $carrito->id)->get();
-            $items = array();
-            $resultado->each(function ($item) use (&$items) {
-                $items[] = $item;
-                \Cart::add([
-                    'idCarrito' => $item->carrito_id,
-                    'id' => $item->id,
-                    'name' => $item->nombre,
-                    'price' => $item->precio,
-                    'quantity' => $item->cantidad,
-                    'attributes' => array(
-                        'image' => $item->imagen,
-                    )
-
-                ]);
-            });
-        }
-    }
 }
